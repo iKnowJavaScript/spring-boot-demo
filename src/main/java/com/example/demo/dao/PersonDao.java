f6315cb4f9ffa0f63bf15cf7107ee9986aa2c0be
Omolayo Victor <vomolayo@seamfix.com>
2020-02-01 08:28:13 +0100

    Add other CRUD method to Person interface

--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -1,4 +1,28 @@
 package com.example.demo.dao;
 
+import com.example.demo.model.Person;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface PersonDao {
+    int insertPerson(
+            @JsonProperty("id") UUID id,
+            @JsonProperty("name") Person person
+    );
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person>  selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
 }
